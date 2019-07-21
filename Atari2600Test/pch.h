ef71a1ae950951ad0a9d5388ced1469b676f0791
Matthew Hinton <matthewhinton56@gmail.com>
2019-07-21 18:47:50 -0500

    Undid test moves

--- Atari2600Test/pch.h
+++ Atari2600Test/pch.h
@@ -9,5 +9,5 @@
 #include "MOS6507/Memory.h"
 #include "MOS6507\MemoryAccessor.h"
 #include "MOS6507\Instructions\Binary.h"
-#include <MOS6507\Instructions\StandardInstructions\StandardInstruction.h>
-#include <MOS6507\Instructions\XInstructions\XDecIncIntructions.h>
+#include "MOS6507\Instructions\StandardInstructions\StandardInstruction.h"
+#include "MOS6507\Instructions\XDecIncInstructions\XDecIncIntructions.h""
