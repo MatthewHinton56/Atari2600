de35bb87db4c042e3176b58d7c6a6e08ddcf639e
Matthew Hinton <matthewhinton56@gmail.com>
2019-07-21 18:34:15 -0500

    Folder restructuring
    Unit tests for XDecInc file

--- Atari2600Test/pch.h
+++ Atari2600Test/pch.h
@@ -10,3 +10,4 @@
 #include "MOS6507\MemoryAccessor.h"
 #include "MOS6507\Instructions\Binary.h"
 #include <MOS6507\Instructions\StandardInstructions\StandardInstruction.h>
+#include <MOS6507\Instructions\XInstructions\XDecIncIntructions.h>
